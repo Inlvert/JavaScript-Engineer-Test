ecc1ba853972879713757e039bb85c9d8d48e57d
Boris Ushakov <inlvert@gmail.com>
2024-10-30 22:53:14 +0200

    add mongoose lib + connection to DB

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const CONSTANTS = require("../constants");
+
+async function connectToDB() {
+  await mongoose.connect(CONSTANTS.DB_URL);
+}
+
+connectToDB().catch((err) => console.log(err));
+
+module.exports = {
+  
+};
