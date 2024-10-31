e8fc97b0878dae8b63baa31b4a2478514d7cea6a
Boris Ushakov <inlvert@gmail.com>
2024-10-31 16:36:11 +0200

    add model validation

--- server/controllers/superhero.controllers.js
+++ server/controllers/superhero.controllers.js
@@ -4,17 +4,24 @@ module.exports.createSuperhero = async (req, res, next) => {
   try {
     const { body, files } = req;
 
-    const images = files.map(file => file.filename)
+    const images = files.map((file) => file.filename);
 
-    const superhero = await Superhero.create({ ...body, images })
+    const superhero = await Superhero.create({ ...body, images });
 
     res.status(201).send({ data: superhero });
+    
   } catch (error) {
+
+    if (error.code === 11000) {
+      return res.status(400).send({ error: "Nickname must be unique." });
+    }
+
     next(error);
   }
 };
 
 module.exports.getSuperheros = async (req, res, next) => {};
+
 module.exports.getSuperhero = async (req, res, next) => {};
 module.exports.updateSuperhero = async (req, res, next) => {};
 module.exports.deleteSuperhero = async (req, res, next) => {};
--- server/models/superhero.js
+++ server/models/superhero.js
@@ -3,13 +3,20 @@ const { Schema, model } = require("mongoose");
 const superheroSchema = new Schema({
   nickname: {
     type: String,
-    required: true,
+    required: [true, "Nickname is required"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9]{1,16}$/,
+      "Nickname mast be 1-16 symbols and must contain letters and numbers only",
+    ],
   },
   realName: {
     type: String,
   },
   originDescription: {
     type: String,
+    default:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore accusamus velit unde laboriosam repellendus suscipit nobis rerum libero at, ea neque impedit distinctio obcaecati delectus ipsam nemo ducimus veniam molestiae!",
   },
   superpowers: {
     type: String,
