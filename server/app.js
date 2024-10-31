c073fb0be6fcf48ee1c7f7d7c3e18012319a189f
Boris Ushakov <inlvert@gmail.com>
2024-10-31 14:39:56 +0200

    add basic error hendler

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require("./routers");
 const cors = require("cors");
+const basicErrorHendler = require("./middlewares/errors/basic")
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(express.static("public"));
+app.use(basicErrorHendler)
 
 module.exports = app;
