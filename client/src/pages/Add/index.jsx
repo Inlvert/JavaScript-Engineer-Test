6479ab66ea2b8e11ff2e0b9599263a80f00c1725
Boris Ushakov <inlvert@gmail.com>
2024-11-01 13:46:24 +0200

    add basic AddSuperhero page

---
+++ client/src/pages/Add/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const AddSuperhero = () => {
+  return (
+    <div>
+      <Header />
+      <h1>AddSuperhero</h1>
+    </div>
+  );
+};
+
+export default AddSuperhero;
