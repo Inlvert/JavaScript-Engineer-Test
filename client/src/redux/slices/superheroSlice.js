48cbd23b6458136a3fc8f3dd6e221fbf2197232a
Boris Ushakov <inlvert@gmail.com>
2024-11-02 19:23:31 +0200

    add AsyncThunk getSuperheroes + reducers

--- client/src/redux/slices/superheroSlice.js
+++ client/src/redux/slices/superheroSlice.js
@@ -5,6 +5,8 @@ const SLICE_NAME = "superheroes";
 
 let initialState = {
   superheroes: [],
+  totalPages: 0,
+  currentPage: 1,
   isLoading: null,
   error: null,
 };
@@ -28,9 +30,32 @@ const createSuperhero = createAsyncThunk(
   }
 );
 
+const getSuperheroes = createAsyncThunk(
+  `${SLICE_NAME}/get`,
+  async (page, thunkAPI) => {
+    try {
+      const response = await API.getSuperheroes(page);
+
+      const { data, totalPages, currentPage } = response;
+
+      return { data, totalPages, currentPage };
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.data.errors);
+    }
+  }
+);
+
 const superheroSlice = createSlice({
   name: SLICE_NAME,
   initialState,
+  reducers: {
+    nextPage: (state) => {
+      state.currentPage = +(state.currentPage) + 1;
+    },
+    prevPage: (state) => {
+      state.currentPage = +(state.currentPage) - 1;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(createSuperhero.pending, (state) => {
       state.isLoading = true;
@@ -43,11 +68,27 @@ const superheroSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     });
+    builder.addCase(getSuperheroes.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getSuperheroes.fulfilled, (state, action) => {
+      const { data, totalPages, currentPage } = action.payload;
+      state.isLoading = false;
+      state.superheroes = data;
+      state.totalPages = totalPages;
+      state.currentPage = currentPage;
+    });
+    builder.addCase(getSuperheroes.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
   },
 });
 
 const { reducer: superheroReducer, actions } = superheroSlice;
 
-export { createSuperhero };
+export const { nextPage, prevPage } = actions;
+
+export { createSuperhero, getSuperheroes };
 
 export default superheroReducer;
