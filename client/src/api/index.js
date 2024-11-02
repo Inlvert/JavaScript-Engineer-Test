cb8ad75e708279bc460ad5aced07f9848ebcc3da
Boris Ushakov <inlvert@gmail.com>
2024-11-02 19:24:20 +0200

    add api getSuperheroes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import CONSTANTS from "../constants";
+import { createAction } from "@reduxjs/toolkit";
 
 const httpClient = axios.create({
   baseURL: CONSTANTS.HTTP_SERVER_URL,
@@ -8,4 +9,13 @@ const httpClient = axios.create({
 export const createSuperhero = async (superheroData) => {
   const response = await httpClient.post(`/superheroes`, superheroData);
   return response;
+};
+
+export const getSuperheroes = async (page) => {
+  const response = await httpClient.get(`/superheroes?page=${page}`);
+  return {
+    data: response.data.data,
+    totalPages: response.data.totalPages,
+    currentPage: +(response.data.currentPage),
+  };
 };
