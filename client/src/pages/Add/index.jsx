8acae4c7f694d116c3df7a12b6f723853732b7c1
Boris Ushakov <inlvert@gmail.com>
2024-11-02 03:53:46 +0200

    add component AddSuperheroForm to page AddSuperhero

--- client/src/pages/Add/index.jsx
+++ client/src/pages/Add/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
+import AddSuperheroForm from "../../components/AddSuperheroForm";
 
 const AddSuperhero = () => {
   return (
     <div>
       <Header />
       <h1>AddSuperhero</h1>
+      <AddSuperheroForm />
     </div>
   );
 };
